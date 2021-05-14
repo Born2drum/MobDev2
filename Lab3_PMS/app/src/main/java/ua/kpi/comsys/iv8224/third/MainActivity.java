fa5b3c50503890394bbb44505f8f1bca584d227b
Born2drum <47678190+Born2drum@users.noreply.github.com>
2021-05-15 00:29:00 +0300

    Lab2-3

---
+++ Lab3_PMS/app/src/main/java/ua/kpi/comsys/iv8224/third/MainActivity.java
@@ -0,0 +1,32 @@
+package ua.kpi.comsys.iv8224.third;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_info, R.id.navigation_dashboard, R.id.navigation_videocam).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+    }
+
+}
